5cae8dfd1e85d78e37d8ad291213d3df0a7b7b50
ZINCChen <56262581+ZINCChen@users.noreply.github.com>
2023-08-17 16:48:17 +0800

    Update listing_3.13.cpp

--- src/listing_3.13.cpp
+++ src/listing_3.13.cpp
@@ -1,4 +1,4 @@
-// 这段代码演示了使用 `boost::shared_mutex`（共享互斥锁）实现线程安全的 DNS 缓存。`shared_mutex` 允许多个线程同时读取共享数据，但只允许一个线程写入数据。下面是代码的详细解释：
+// 这段代码实现了一个线程安全的 DNS 缓存类，使用了 `std::map` 存储 DNS 条目，并使用 `boost::shared_mutex` 来实现读写锁。以下是代码的逐行注释和解释：
 
 // ```cpp
 #include <map>
@@ -13,7 +13,9 @@ class dns_cache
 {
     std::map<std::string, dns_entry> entries;  // 存储 DNS 条目的映射
     boost::shared_mutex entry_mutex;  // 共享互斥锁，用于保护 entries 的访问
+    
 public:
+    // 查找给定域名的 DNS 条目
     dns_entry find_entry(std::string const& domain)
     {
         boost::shared_lock<boost::shared_mutex> lk(entry_mutex);  // 使用 shared_lock 允许多个线程同时读取
@@ -21,6 +23,8 @@ public:
             entries.find(domain);
         return (it == entries.end()) ? dns_entry() : it->second;  // 查找并返回 DNS 条目
     }
+    
+    // 更新或添加给定域名的 DNS 条目
     void update_or_add_entry(std::string const& domain,
                              dns_entry const& dns_details)
     {
@@ -33,8 +37,16 @@ int main()
 {}
 // ```
 
-// 在这段代码中，`dns_entry` 表示 DNS 条目，`dns_cache` 是 DNS 缓存类。`entries` 是一个 `std::map`，用于存储域名和对应的 DNS 条目。`entry_mutex` 是 `boost::shared_mutex`，用于保护对 `entries` 的访问。
+// **输入输出实例：**
 
-// 在 `dns_cache` 类中，`find_entry` 函数使用 `boost::shared_lock` 进行上锁，允许多个线程同时读取 DNS 条目。通过查找域名对应的条目，返回相应的 `dns_entry`。`update_or_add_entry` 函数使用 `std::lock_guard` 进行上锁，确保在更新或添加条目时只有一个线程执行写入操作。
+// 由于这段代码主要是关于线程安全性和并发控制的示例，没有具体的输入和输出操作。但是，我们可以用一些例子来说明如何使用该代码。
 
-// 这种设计能够实现高效的读取和安全的写入操作，允许多个线程同时读取 DNS 条目，但只允许一个线程写入条目。这可以提高性能并保证数据的一致性。
+// **实际应用例子：**
+
+// 1. **网络服务器中的 DNS 缓存：** 在一个网络服务器中，有许多客户端需要查询域名的 DNS 记录。使用这个线程安全的 DNS 缓存类可以确保多个线程可以同时查询 DNS 条目，而只有一个线程可以更新或添加 DNS 条目，保证了数据的一致性和线程安全。
+
+// 2. **多线程 Web 爬虫：** 在一个多线程的 Web 爬虫中，不同的线程可能同时访问同一个域名的 DNS 条目。使用这个线程安全的 DNS 缓存类可以避免竞态条件，确保多个线程可以同时访问已缓存的 DNS 数据。
+
+// 3. **分布式系统的节点管理：** 在一个分布式系统中，不同的节点可能需要访问其他节点的 IP 地址。使用这个线程安全的 DNS 缓存类可以在节点管理中保证数据的一致性，并确保多个节点可以安全地查询和更新 DNS 条目。
+
+// 总之，这段代码展示了如何使用 `boost::shared_mutex` 实现线程安全的读写操作，适用于多线程并发访问的场景，以提高性能并保证数据的一致性。
