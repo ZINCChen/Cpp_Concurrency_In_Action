089e2a8dce7500b0b1a027eb94b1fe4b776c5a25
ZINCChen <56262581+ZINCChen@users.noreply.github.com>
2023-08-17 16:43:47 +0800

    Update listing_3.13.cpp

--- src/listing_3.13.cpp
+++ src/listing_3.13.cpp
@@ -1,3 +1,6 @@
+// 这段代码演示了使用 `boost::shared_mutex`（共享互斥锁）实现线程安全的 DNS 缓存。`shared_mutex` 允许多个线程同时读取共享数据，但只允许一个线程写入数据。下面是代码的详细解释：
+
+// ```cpp
 #include <map>
 #include <string>
 #include <mutex>
@@ -8,23 +11,30 @@ class dns_entry
 
 class dns_cache
 {
-    std::map<std::string,dns_entry> entries;
-    boost::shared_mutex entry_mutex;
+    std::map<std::string, dns_entry> entries;  // 存储 DNS 条目的映射
+    boost::shared_mutex entry_mutex;  // 共享互斥锁，用于保护 entries 的访问
 public:
     dns_entry find_entry(std::string const& domain)
     {
-        boost::shared_lock<boost::shared_mutex> lk(entry_mutex);
-        std::map<std::string,dns_entry>::const_iterator const it=
+        boost::shared_lock<boost::shared_mutex> lk(entry_mutex);  // 使用 shared_lock 允许多个线程同时读取
+        std::map<std::string, dns_entry>::const_iterator const it =
             entries.find(domain);
-        return (it==entries.end())?dns_entry():it->second;
+        return (it == entries.end()) ? dns_entry() : it->second;  // 查找并返回 DNS 条目
     }
     void update_or_add_entry(std::string const& domain,
                              dns_entry const& dns_details)
     {
-        std::lock_guard<boost::shared_mutex> lk(entry_mutex);
-        entries[domain]=dns_details;
+        std::lock_guard<boost::shared_mutex> lk(entry_mutex);  // 使用 lock_guard 进行写入操作的互斥保护
+        entries[domain] = dns_details;  // 更新或添加 DNS 条目
     }
 };
 
 int main()
 {}
+// ```
+
+// 在这段代码中，`dns_entry` 表示 DNS 条目，`dns_cache` 是 DNS 缓存类。`entries` 是一个 `std::map`，用于存储域名和对应的 DNS 条目。`entry_mutex` 是 `boost::shared_mutex`，用于保护对 `entries` 的访问。
+
+// 在 `dns_cache` 类中，`find_entry` 函数使用 `boost::shared_lock` 进行上锁，允许多个线程同时读取 DNS 条目。通过查找域名对应的条目，返回相应的 `dns_entry`。`update_or_add_entry` 函数使用 `std::lock_guard` 进行上锁，确保在更新或添加条目时只有一个线程执行写入操作。
+
+// 这种设计能够实现高效的读取和安全的写入操作，允许多个线程同时读取 DNS 条目，但只允许一个线程写入条目。这可以提高性能并保证数据的一致性。
